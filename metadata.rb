38133cdd8f6f145a018e36be302c7b4499c245d3
Leandro Di Tommaso <leandro.ditommaso@mikroways.net>
2014-09-12 11:20:20 -0300

    Initial commit.

---
+++ metadata.rb
@@ -0,0 +1,8 @@
+name             'cespi_server_base'
+maintainer       'Christian A. Rodriguez & Leandro Di Tommaso'
+maintainer_email 'car@cespi.unlp.edu.ar lditommaso@cespi.unlp.edu.ar'
+license          'All rights reserved'
+description      'Installs/Configures cespi_server_base'
+long_description 'Installs/Configures cespi_server_base'
+version          '0.1.0'
+
