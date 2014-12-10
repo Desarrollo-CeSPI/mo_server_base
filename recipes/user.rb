1987ff089c2f0fdf326581b40c795988141ea07f
Leandro Di Tommaso <leandro.ditommaso@mikroways.net>
2014-12-10 09:49:23 -0300

    Removed uid and gid.
    UID and GID were hardcoded in attributes and in sample data bag.

--- recipes/user.rb
+++ recipes/user.rb
@@ -1,12 +1,12 @@
 include_recipe "users"
 
 users_manage node[:mo_server_base][:authorization][:superadmin_group] do
-  group_id 1200
+#  group_id 1200
   action :create
 end
 
 users_manage "devops" do
-  group_id 1201
+#  group_id 1201
   action :create
 end
 
