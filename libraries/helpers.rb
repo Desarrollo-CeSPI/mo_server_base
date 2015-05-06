86b861c9a2e7a0cccaa48edc50372c71a56ab5be
Leandro Di Tommaso <leandro.ditommaso@mikroways.net>
2015-05-06 18:45:55 -0300

    Modified helper method mo_server_base_superadmin_user.
    By default was returning user id, but sometimes user id and LDAP uid for
    that user were different, so now it is possible to specify the parameter
    that is needed.

--- libraries/helpers.rb
+++ libraries/helpers.rb
@@ -1,9 +1,9 @@
-def mo_server_base_superadmin_users
+def mo_server_base_superadmin_users(attribute="id")
   data_bag('users').map do |user|
     data_bag_item('users', user)
   end.select do |user|
     user["groups"].include? node[:mo_server_base][:authorization][:superadmin_group]
   end.map do |user|
-    user["id"]
+    user[attribute] || user["id"]
   end
 end
